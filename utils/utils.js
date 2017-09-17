bbbce8737fd0717ef193f5778cf966bbfb8cc864
Mike McGarry <mikemcgarry@live.com.au>
2017-09-17 14:00:52 +1000

    Completed testing

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,21 @@
 module.exports.add = (a,b) => a+b
+
 module.exports.square = (a) => a*a
+
+module.exports.setName = (user, fullName) => {
+  var names = fullName.split(' ')
+  user.firstName = names[0]
+  user.lastName = names[1]
+  return user
+}
+
+module.exports.addASynch = (a,b,cb) => {
+  setTimeout(() => {
+    cb(a+b)}, 1800)
+}
+
+module.exports.squareASynch = (a, cb) => {
+  setTimeout(() => {
+    cb(a*a)
+  }, 1000)
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -5,15 +5,31 @@ const expect = require('expect')
 
 it('Should add two numbers', () => {
   var result = utils.add(5,10)
-  expect(result).toBe(16)
-  //if (result != 15) {
-    //throw new Error(`expected 15 got ${result}`)
-  //}
+  expect(result).toBe(15)
 })
 
 it('Should square the number', () => {
   var result = utils.square(10)
-  if (result != 100) {
-    throw new Error(`expected 100 got ${result}`)
+  expect(result).toBe(100)
   }
+)
+
+it('Should split the names', () => {
+  var user = utils.setName({}, "Mike McGarry")
+  expect(user).toHaveProperty("firstName", "Mike")
+  expect(user).toHaveProperty("lastName", "McGarry")
+})
+
+it('Should add two numbers asynch', (done) => {
+  var result = utils.addASynch(5,10, (sum) => {
+    expect(sum).toBe(15)
+    done()
+  })
+})
+
+it('Should square the number asynch', (done) => {
+  var result = utils.squareASynch(5, (square) => {
+    expect(square).toBe(25)
+    done()
+  })
 })
