4e7b2e1ade7d351d257a594fc4c880fb13e71d07
Mike McGarry <mikemcgarry@live.com.au>
2017-09-17 13:07:44 +1000

    Updated

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,10 +1,14 @@
 const utils = require('./utils')
+const expect = require('expect')
+
+
 
 it('Should add two numbers', () => {
   var result = utils.add(5,10)
-  if (result != 15) {
-    throw new Error(`expected 15 got ${result}`)
-  }
+  expect(result).toBe(16)
+  //if (result != 15) {
+    //throw new Error(`expected 15 got ${result}`)
+  //}
 })
 
 it('Should square the number', () => {
